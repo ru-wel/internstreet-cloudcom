e32fa57944aa3ff1d2447a1349b56f6d7cd116a7
welru <reuelchristian.sundiam04@gmail.com>
2025-03-07 21:27:17 +0800

    added login form on frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/userPages/Home';
 import './App.css';
+import Home from './pages/userPages/Home';
 import JobListing from './pages/userPages/JobListing';
 import Jobs from './pages/adminPages/Jobs';
+import Login from './pages/userPages/Login';
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/job-listing" element={<JobListing/>} />
         <Route path="/jobs" element={<Jobs/>} />
+        <Route path="/login" element={<Login/>} />
       </Routes>
     </Router>
   )
