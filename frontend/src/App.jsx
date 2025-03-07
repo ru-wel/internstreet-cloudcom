cdd95d6a0d503fc63edd6e5c0443d75d8d972a9b
Yuji <mallarilevin@gmail.com>
2025-03-07 21:25:57 +0800

    changed routing for admin pages + adminlogs page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,20 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/userPages/Home';
 import './App.css';
 import JobListing from './pages/userPages/JobListing';
-import Jobs from './pages/adminPages/Jobs';
+import AdminJobs from './pages/adminPages/AdminJobs';
+import AdminUsers from './pages/adminPages/AdminUsers';
+import AdminLogs from './pages/adminPages/AdminLogs';
 
 function App() {
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={< Home />} />
         <Route path="/job-listing" element={<JobListing/>} />
-        <Route path="/jobs" element={<Jobs/>} />
+        <Route path="/admin-jobs" element={<AdminJobs/>} />
+        <Route path="/admin-users" element={<AdminUsers/>} />
+        <Route path="/admin-logs" element={<AdminLogs/>} />
       </Routes>
     </Router>
   )
