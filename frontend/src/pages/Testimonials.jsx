8029fdee70d93259223952165f7981b6b7f1c524
Ethan James Gonzales <ethanjamesgonzales088@gmail.com>
2025-03-08 20:30:40 +0800

    testimonials added

---
+++ frontend/src/pages/Testimonials.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import person from "../assets/person-placeholder.png"
+
+const testimonials = [
+  {
+    image: person,
+    message: "Starting my role as a WordPress administrator has been a joy, thanks to its intuitive interface, media management, security, and plugin integration, making website management a breeze.",
+    rating: 5,
+  },
+  {
+    image: person,
+    message: "Starting my role as a WordPress administrator has been a joy, thanks to its intuitive interface, media management, security, and plugin integration, making website management a breeze.",
+    rating: 5,
+  },
+  {
+    image: person,
+    message: "Starting my role as a WordPress administrator has been a joy, thanks to its intuitive interface, media management, security, and plugin integration, making website management a breeze.",
+    rating: 5,
+  },
+];
+
+const Testimonials = () => {
+  return (
+    <div className="bg-[#D5E1DF] py-18 px-6 text-center">
+      <h2 className="text-6xl font-bold mb-12">Testimonials</h2>
+      
+      <div className="grid md:grid-cols-3 gap-8 max-w-5xl mx-auto">
+        {testimonials.map((testimonial, index) => (
+          <div key={index} className="p-6 flex flex-col items-center">
+            <img src={testimonial.image} alt="Testimonial" className="w-20 h-20 rounded-full border-2 mb-4" />
+            <p className="text-gray-800 text-lg italic my-5">"{testimonial.message}"</p>
+            <div className="text-yellow-500 text-4xl">
+              {"★".repeat(testimonial.rating)}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Testimonials;
--- frontend/src/pages/userPages/Landing.jsx
+++ frontend/src/pages/userPages/Landing.jsx
@@ -3,6 +3,7 @@ import Nav from '../Nav'
 import Hero from '../Hero'
 import Marquee from '../Marquee'
 import Benefits from '../Benefits'
+import Testimonials from '../Testimonials'
 
 function Landing() {
   return (
@@ -11,6 +12,7 @@ function Landing() {
       <Hero></Hero>
       <Marquee></Marquee>
       <Benefits></Benefits>
+      <Testimonials></Testimonials>
     </div>
   )
 }
