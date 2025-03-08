28f254ed74212b549264798e04cb9f9c1d10fe99
Ethan James Gonzales <ethanjamesgonzales088@gmail.com>
2025-03-08 19:57:17 +0800

    marquee companies done

--- frontend/src/pages/userPages/Landing.jsx
+++ frontend/src/pages/userPages/Landing.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Nav from '../Nav'
 import Hero from '../Hero'
-import Job from './Job'
+import Marquee from '../Marquee'
 
 function Landing() {
   return (
     <div>
       <Nav></Nav>
       <Hero></Hero>
+      <Marquee></Marquee>
     </div>
   )
 }
