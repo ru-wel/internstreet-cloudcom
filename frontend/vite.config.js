1be41674506818de09fba05640c7aeb270a6f3af
welru <reuelchristian.sundiam04@gmail.com>
2025-03-27 10:18:43 +0800

    vite config for production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,14 +5,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // server: {
-  //   host: '0.0.0.0',
-  //   port: 5173,
-  //   watch: {
-  //     usePolling: true
-  //   },
-  //   origin: 'http://internstreetph.site',
-  //   allowedHosts: ['internstreetph.site', 'www.internstreetph.site'],
-  //   hmr: { clientPort: 5173 }
-  // }
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    watch: {
+      usePolling: true
+    },
+    origin: 'http://internstreetph.site',
+    allowedHosts: ['internstreetph.site', 'www.internstreetph.site'],
+    hmr: { clientPort: 5173 }
+  }
 })
